c67a48d57b50249da86d7936dd511edd0359fe5b
myronn8330 <91744974+myronn8330@users.noreply.github.com>
2023-05-24 07:56:54 -0400

    hp

--- script.js
+++ script.js
@@ -58,46 +58,6 @@ function check(symbol) {
         $(".sq8").css("color", "green");
         $(".sq9").css("color", "green");
         return true;
-    } else if ($(".sq1").hasClass(symbol)
-            && $(".sq4").hasClass(symbol)
-            && $(".sq7").hasClass(symbol))
-    {
-        $(".sq1").css("color", "green");
-        $(".sq4").css("color", "green");
-        $(".sq7").css("color", "green");
-        return true;
-    } else if ($(".sq2").hasClass(symbol)
-            && $(".sq5").hasClass(symbol)
-            && $(".sq8").hasClass(symbol))
-    {
-        $(".sq2").css("color", "green");
-        $(".sq5").css("color", "green");
-        $(".sq8").css("color", "green");
-        return true;
-    } else if ($(".sq3").hasClass(symbol)
-            && $(".sq6").hasClass(symbol)
-            && $(".sq9").hasClass(symbol))
-    {
-        $(".sq3").css("color", "green");
-        $(".sq6").css("color", "green");
-        $(".sq9").css("color", "green");
-        return true;
-    } else if ($(".sq1").hasClass(symbol)
-            && $(".sq5").hasClass(symbol)
-            && $(".sq9").hasClass(symbol))
-    {
-        $(".sq1").css("color", "green");
-        $(".sq5").css("color", "green");
-        $(".sq9").css("color", "green");
-        return true;
-    } else if ($(".sq3").hasClass(symbol)
-            && $(".sq5").hasClass(symbol)
-            && $(".sq7").hasClass(symbol))
-    {
-        $(".sq3").css("color", "green");
-        $(".sq5").css("color", "green");
-        $(".sq7").css("color", "green");
-        return true;
     } else {
         return false;
     }
